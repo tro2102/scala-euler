325b02a430ecef6db20e0dbace18da1aab7580a9
Taylor Owens <taylor.owens@pegs.com>
2014-08-28 16:52:14 -0700

    Problem 2

---
+++ src/main/scala/com/github/tro2102/euler/math/Fibonacci.scala
@@ -0,0 +1,16 @@
+package com.github.tro2102.euler.math
+
+/**
+ * Created on 8/28/14.
+ * @author Taylor Owens
+ */
+object Fibonacci {
+
+  def generateUpTo(initList: List[Int], maxValue: Int): List[Int] = {
+    val nextValue = if (initList.size < 2) 1 else initList(0) + initList(1)
+    if (nextValue > maxValue)
+      initList
+    else generateUpTo(initList.+:(nextValue), maxValue)
+  }
+
+}
--- src/main/scala/com/github/tro2102/euler/runner/ProblemRunner.scala
+++ src/main/scala/com/github/tro2102/euler/runner/ProblemRunner.scala
@@ -1,6 +1,6 @@
 package com.github.tro2102.euler.runner
 
-import com.github.tro2102.euler.math.DivisorCalculator
+import com.github.tro2102.euler.math.{Fibonacci, DivisorCalculator}
 
 /**
  * Created on 8/28/14.
@@ -22,11 +22,16 @@ object ProblemRunner {
   def apply(num: Int): ProblemRunner = {
     num match {
       case 1 => new Problem1
+      case 2 => new Problem2
       case _ => throw new NotImplementedError(s"No implementation for Problem #$num")
     }
   }
   private class Problem1 extends ProblemRunner {
     override def runImpl: String =
       DivisorCalculator.Range.sum(1 until 1000, 3, 5).toString
+  }
+  private class Problem2 extends ProblemRunner {
+    override def runImpl: String =
+      DivisorCalculator.List.sum(Fibonacci.generateUpTo(List(2,1), 4000000), 2).toString
   }
 }
